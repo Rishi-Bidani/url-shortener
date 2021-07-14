b8c97acaa4044edb0f20513d9f02174c22eedf53
Rishi <rishibidani12@gmail.com>
2021-07-14 18:34:02 +0530

    basic link input, key generation and link output

--- backendjs/mongo.js
+++ backendjs/mongo.js
@@ -1,82 +1,8 @@
 const mongodb = require("mongodb");
 const path = require("path");
 const {KeyGen} = require("./KeyGen");
-
 // Environment Variables
 const dotenv = require("dotenv").config({path: path.resolve(__dirname, "../.env")});
-const password = process.env.DB_PASS;
-const dbname = process.env.DB_NAME;
-const user = process.env.DB_USER;
-// ================================================================================================================= //
-const uri = `mongodb+srv://${user}:${password}@cluster0.oqyh2.mongodb.net/shortener?retryWrites=true&w=majority`
-
-// async function urldata() {
-//     const client = await mongodb.MongoClient.connect
-//     (uri, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     return client.db(dbname).collection("urls")
-// }
-
-//
-// async function test() {
-//     const urls = await urldata();
-//     const data = await urls.find({}).toArray()
-//     console.log(data)
-//     await urls.insertOne({
-//         short: "hello2",
-//         full: "The full url 2"
-//     });
-// }
-
-/* =========================================================
-*   This function takes a parameter of the short url
-*   and returns whether the url/key exists and is unique or not
-* ==========================================================*/
-// async function checkKey(short) {
-//     const urls = await urldata();
-//     const data = await urls.find({short: short}).toArray();
-//     if (data.length === 1) {
-//         return {unique: true, data: data[0].full}
-//     } else if (data.length > 1) {
-//         return {unique: false, data: "multiple"}
-//     } else {
-//         return {unique: false, data: "N/A"}
-//     }
-// }
-//
-// async function genKey() {
-//     const key = new KeyGen().returnKey();
-//     const {unique, data} = await checkKey(key);
-//     console.log()
-//     if (!unique) {
-//         return key
-//     } else {
-//         await genKey()
-//     }
-// }
-//
-// async function writeKey(short, long) {
-//     const urls = await urldata();
-//     const finish = await urls.insertOne({
-//         short: short,
-//         full: long
-//     })
-//     return finish
-// }
-//
-// genKey().then(key => {
-//     writeKey(key, "thisWillBeTheLongOne").then(res => console.log(res))
-// })
-
-// test()
-// checkKey("hello2")
-// const key = new KeyGen();
-// console.log(key.returnKey())
-
-// Return unique key
-//
 
 class UseMongo {
     constructor() {
@@ -132,9 +58,11 @@ class UseMongo {
 
 }
 
-const usemongo = new UseMongo();
-usemongo.genKey().then(key=>{
-    usemongo.writeKey(key, "TestingClassInsert").then(data=>{
-        console.log(data)
-    })
-})
+// const usemongo = new UseMongo();
+// usemongo.genKey().then(key => {
+//     usemongo.writeKey(key, "TestingClassInsert").then(data => {
+//         console.log(data)
+//     })
+// })
+
+module.exports = {UseMongo}
--- main.js
+++ main.js
@@ -3,7 +3,33 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const path = require("path");
+const {UseMongo} = require("./backendjs/mongo");
+const usemongo = new UseMongo()
+
 app.set("views", path.join(__dirname, "templates"));
+app.use(express.static("static"))
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/posts/submit-link", async (req, res) => {
+    const {fullLink} = req.body;
+    console.log(fullLink);
+    usemongo.genKey().then((key) => {
+        console.log(key)
+        // res.end();
+        usemongo.writeKey(key, fullLink).then(async (data) => {
+            console.log(data)
+            res.json(key).end();
+        })
+    })
+})
 
 
 const port = process.env.PORT || 5000;

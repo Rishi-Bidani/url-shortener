504910e5fcaa7d4c0c3c5ff52fa73f8a8b6eb8cf
Rishi <rishibidani12@gmail.com>
2021-07-16 09:34:15 +0530

    created json of country codes, adding login page
    Refined the css code a bit, by breaking it up

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const app = express();
 const http = require("http").Server(app);
 const path = require("path");
 const posts = require("./routes/posts");
+const geoip = require('geoip-lite');
+const codeToCountry = require('./database/countryCodeData.json');
 
 app.set("views", path.join(__dirname, "templates"));
 app.use(express.static("static"))
@@ -19,8 +21,20 @@ app.use("/posts", posts.router);
 app.get("/", (req, res) => {
     res.render("index")
 })
+app.get("/login", (req, res)=>{
+    res.render("login")
+})
+
+app.get('/ip', (req, res) => {
+    const ip = req.ip;
+    // console.log(ip); // ip address of the user
+    const geo = geoip.lookup("123.161.170.57");
+    console.log(geo)
+    console.log(codeToCountry[geo.country])
+    res.end()
+});
 
 const port = process.env.PORT || 5000;
-http.listen(port, () => {
+http.listen(port,"0.0.0.0", () => {
     console.log("Website live on: " + `http://localhost:${port}`);
 });
